61835e4920ec961de5347fc96c4210f092aa0f5d
Tomas Ospina Gaviria <tospinag@unal.edu.co>
2025-01-14 17:49:32 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -1,5 +1,53 @@
 package comunicacion;
 
-public class Alfabeto {
+public class Alfabeto extends Escrito {
+    private String[] letras;
+    private String interpretacion;
 
+    public Alfabeto(String origen, String[] letras, String interpretacion) {
+        super(origen, null, null, letras.length);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+
+    public Alfabeto() {
+        super("", "", "", 0);
+        this.letras = new String[0];
+        this.interpretacion = "";
+    }
+
+    public String[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(String[] letras) {
+        this.letras = letras;
+    }
+
+    public String getInterpretacion() {
+        return interpretacion;
+    }
+
+    public void setInterpretacion(String interpretacion) {
+        this.interpretacion = interpretacion;
+    }
+
+    public int cantidadLetras() {
+        return letras.length;
+    }
+
+    @Override
+    public int palabrasTotales(int factor) {
+        return cantidadLetras();
+    }
+
+    @Override
+    public String interpretacion() {
+        return interpretacion;
+    }
+
+    @Override
+    public String toString() {
+        return String.join(", ", letras);
+    }
 }
