1f5510af37325e294d17d1fc512a814c0aa570ca
Tomas Ospina Gaviria <tospinag@unal.edu.co>
2025-01-14 17:40:08 -0500

    Some changes

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,35 @@
+package comunicacion;
+
+class Fabula extends Escrito {
+    private String enseñanza;
+    private String interpretacion;
+
+    public Fabula(String origen, String titulo, String autor, int paginas, String enseñanza, String interpretacion) {
+        super(origen, titulo, autor, paginas);
+        this.enseñanza = enseñanza;
+        this.interpretacion = interpretacion;
+    }
+
+    public String getEnseñanza() {
+        return enseñanza;
+    }
+
+    public void setEnseñanza(String enseñanza) {
+        this.enseñanza = enseñanza;
+    }
+
+    @Override
+    public int palabrasTotales(int palabrasPagina) {
+        return getPaginas() * palabrasPagina * 1;
+    }
+
+    @Override
+    public String interpretacion() {
+        return interpretacion;
+    }
+
+    @Override
+    public String toString() {
+        return getOrigen() + "\n" + getTitulo() + "\n" + getAutor() + "\n" + getPaginas() + "\n" + enseñanza;
+    }
+}
